499fc511e82624e5322ed5cc4e04f447dc2508ba
Ann 杏 Kilzer <akilzer@gmail.com>
2021-11-06 00:06:53 +0900

    add art detail page

---
+++ pages/artDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{name}}</h1>
+    <img src="http://placekitten.com/700/500" />
+    <span>{{date}}</span><br>
+    <span>{{medium}}</span><br>
+    <span>{{about}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      name: "Artwork name",
+      date: "2020",
+      medium: "oil on canvas",
+      about: "about",
+    };
+  },
+});
+</script>
