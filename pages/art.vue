a7e4133652e35c534662a60c0e754c2c385cccaa
Ann 杏 Kilzer <akilzer@gmail.com>
2021-11-05 23:47:29 +0900

    cat layout

--- pages/art.vue
+++ pages/art.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4">
-      <div v-for="i in boxes" :key="i" container class="bg-blue-100 box-border h-32 w-32 p-4 border-4">{{i}}</div>
+      <div v-for="i in boxes" :key="i" container class="border-yellow-200 box-border border-4">
+        <img src="http://placekitten.com/300/300"/>
+      </div>
     </div>
   </div>
 </template>
