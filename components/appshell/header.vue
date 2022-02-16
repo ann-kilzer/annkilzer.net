1ba89fb3f44b2143acb9e5e9027ac46b17b72c79
Ann 杏 Kilzer <akilzer@gmail.com>
2022-02-16 23:11:21 +0900

    copy content from old site

--- components/appshell/header.vue
+++ components/appshell/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container w-max flex m-4 justify-between">
-    <span>Ann Kilzer</span>
+    <a href="/"><span>Ann Kilzer</span></a>
     <nav v-for="link in links" :key="link.name">
       <a :href="link.path" class="m-2">{{ link.name }}</a>
     </nav>
@@ -14,8 +14,7 @@ export default Vue.extend({
   data() {
     return {
       links: [
-        { name: "Home", path: "/" },
-        { name: "About", path: "/about" },
+        { name: "Software", path: "/software" },
         { name: "Fine Art", path: "/art" },
       ],
     };
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <span>index</span>
+  <div>
+      <h1 class="">
+          Hello!
+        </h1>
+        <p>
+          My name is Ann Kilzer. I'm a polymath, and my career is in software engineering.
+          I am currently a software engineer at
+          <a
+            target="_blank"
+            href="https://www.mercari.com/jp/"
+          >
+            Mercari, Japan
+          </a>.
+        </p>
+        <p>
+          I received my Master's in Computer Science from the University of Texas at Austin,
+          where I spent a few years researching privacy. I grew up in Missoula, Montana, and
+          attended college at Gonzaga University, where I majored in computer science, math,
+          and art. I moved to Tokyo in early 2018 with no job, just an eager dream of living abroad.
+          In my spare time, I enjoy Japanese Calligraphy (Shodo), Nihonga painting, and
+          riding slow trains around Japan.
+        </p>
+        <p>
+          初めまして！杏マリーキルザーです。東京にすんでいます。日本画を描くことと、書道をすることが大好きです。
+          電車も大好きです🚂！よろしくお願いします。
+        </p>
+
+  </div>
 </template>
 
 <script lang="ts">
